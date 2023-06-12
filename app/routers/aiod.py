76a006621cce8fe878333c8140be37065eac56b2
mtkinit <martin.tamajka@kinit.sk>
2023-06-12 16:27:13 +0200

    Add endpoint for single dataset and publication

--- app/routers/aiod.py
+++ app/routers/aiod.py
@@ -16,6 +16,15 @@ async def get_datasets(offset: int = 0, limit: int = settings.DEFAULT_RESPONSE_L
     return res.json()
 
 
+@router.get("/datasets/{id}")
+async def get_datasets(id: int):
+    async_client = aiod_client_wrapper()
+    res = await async_client.get(
+        f"{settings.AIOD_API.BASE_URL}/datasets/{settings.AIOD_API.DATASETS_VERSION}/{id}",
+    )
+    return res.json()
+
+
 @router.get("/publications")
 async def get_publications(
     offset: int = 0, limit: int = settings.DEFAULT_RESPONSE_LIMIT
@@ -24,5 +33,14 @@ async def get_publications(
     res = await async_client.get(
         f"{settings.AIOD_API.BASE_URL}/publications/{settings.AIOD_API.PUBLICATIONS_VERSION}",
         params={"offset": offset, "limit": limit},
+    )
+    return res.json()
+
+
+@router.get("/publications/{id}")
+async def get_datasets(id: int):
+    async_client = aiod_client_wrapper()
+    res = await async_client.get(
+        f"{settings.AIOD_API.BASE_URL}/publications/{settings.AIOD_API.PUBLICATIONS_VERSION}/{id}",
     )
     return res.json()
