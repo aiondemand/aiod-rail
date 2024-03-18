4b7e92aeacee87b94dfe57c24fe7a9c3f717fd85
mtkinit <78868366+mtkinit@users.noreply.github.com>
2024-03-18 14:20:27 +0100

    Feature/debugging (#23)
    * Enable debugging of dev env running in Docker (docker-compose); Debug launch config for VS Code
    * Development environment and debugging readme
    * Added manual/local backend debug, added direct connection to host machine for backend container
    ---------
    Co-authored-by: marcel-vesely-kinit <marcel.vesely@kinit.sk>

---
+++ frontend/src/environments/environment.local-docker.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    BACKEND_API_URL: '/api/v1',
+    DEFAULT_PAGE_SIZE: 10,
+    AIOD_KEYCLOAK_URL: 'https://aiod-dev.i3a.es/aiod-auth',
+    AIOD_KEYCLOAK_REALM: 'aiod',
+    AIOD_KEYCLOAK_CLIENT_ID: 'rail-public'
+};
---
+++ run-dev-docker-env.sh
@@ -0,0 +1,2 @@
+# Starts the development environment using docker-compose
+docker-compose -f docker-compose.yml -f docker-compose-dev.yml up -d --build
