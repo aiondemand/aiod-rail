5b656d76997d83472001d7eb3a143377f5a2f67d
mtkinit <martin.tamajka@kinit.sk>
2024-05-24 09:38:16 +0200

    Docs for updated get_current_user

--- backend/app/authentication.py
+++ backend/app/authentication.py
@@ -33,9 +33,27 @@ def get_current_user(
     from_token: bool = True,
     from_api_key: bool = False,  # By default only for users authenticated through OIDC
 ) -> Callable[[str, str], Awaitable[dict | None]]:
+    """
+    Get the current user based on the provided token or API key (returns an async function).
+    If both token and api_key are defined, the token will be used.
+    
+    Args:
+        required (bool): Whether the user is required to be authenticated.
+        from_token (bool): Whether the user can be authenticated through OIDC.
+        from_api_key (bool): Whether the user can be authenticated through an API key.
+    """
+    
     async def _get_user(
         token: str = Security(oidc), api_key: str = Security(api_key_header)
     ) -> dict | None:
+        """
+        Get the current user based on the provided token or API key.
+        If both token and api_key are defined, the token will be used.
+        
+        Args:
+            token (str): The token provided by the user.
+            api_key (str): The API key provided by the user.
+        """
         if not from_token and not from_api_key:
             raise ValueError("Either from_token or from_api_key must be set to True")
 
