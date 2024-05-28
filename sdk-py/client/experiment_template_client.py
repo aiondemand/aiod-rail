26e465a525d0d072d5c15a70ebe035137f214d54
Eduard Mittas <eduard.mittas@kinit.sk>
2024-05-28 13:37:25 +0200

    added new methods, updated and changed method parameters and descriptions

--- sdk-py/client/experiment_template_client.py
+++ sdk-py/client/experiment_template_client.py
@@ -78,13 +78,19 @@ class ExperimentsTemplates:
                 raise(f'Exception {e}')
 
     def count(self, 
-              include_mine: bool = True, 
-              include_approved: bool = False) -> int:
+              query: str = '', 
+              mine: bool = True, 
+              finalized: bool = True, 
+              approved: bool = True, 
+              public: bool = True) -> int:
         """
             Gets experiment templates count.
             Args:
-                include_mine (bool, optional): If own personal experiment templates should be included in count. Defaults to True.
-                include_approved (bool, optional): If already approved experiments should be counted as well. Defaults to False.
+                query (str, optional): Query used to filter experiment templates. Defaults to empty string, which means that by default count is not filtered.
+                mine (bool, optional): If own personal experiment templates should be counted. Defaults to True.
+                finalized (bool, optional): If experiment templates that are succesfully build and ready to use should be counted as well. Defaults to True.
+                approved (bool, optional): If already approved experiments should be counted as well. Defaults to True.
+                public (bool, optional): If experiment templates flagged as public should be counted as well. Defaults to True.
 
             Returns:
                 int: Number of experiment templates.
@@ -94,21 +100,32 @@ class ExperimentsTemplates:
             
             try:
                 api_response = api_instance.get_experiment_templates_count_v1_count_experiment_templates_get(
-                    include_mine=include_mine, include_approved=include_approved
+                    query=query,
+                    mine=mine, 
+                    approved=approved,
+                    public=public,
+                    finalized=finalized,
                 )
                 return api_response
             except Exception as e:
                 raise(f'Exception {e}')
 
-    def get(self, include_mine: bool = True, 
-            include_approved: bool = True, 
+    def get(self,
+            query: str = '', 
+            mine: bool = True, 
+            finalized: bool = True,
+            approved: bool = True,
+            public: bool = True, 
             offset: int = 0, 
             limit: int = 100) -> list[aiod_rail_sdk.ExperimentTemplateResponse]:
         """
             Gets experiment templates in specified range.
             Args:
-                include_mine (bool, optional): If own personal experiment templates should be included. Defaults to True.
-                include_approved (bool, optional): If already approved experiments should be listed as well. Defaults to True.
+                query (str, optional): Query used to filter experiment templates. Defaults to empty string, which means that by default, it's not used.
+                mine (bool, optional): If own personal experiment templates should be included. Defaults to True.
+                finalized (bool, optional): If experiment templates that are succesfully build and ready to use should be listed as well. Defaults to True.
+                approved (bool, optional): If already approved experiments should be listed as well. Defaults to True.
+                public (bool, optional): If experiment templates flagged as public should be listed as well. Defaults to True.
                 offset (int, optional): Starting index of experiment template range from which to retrieve Defaults to 0.
                 limit (int, optional): Ending index of experiment template range to which to retrieve. Defaults to 100.
 
@@ -120,7 +137,13 @@ class ExperimentsTemplates:
             
             try:
                 api_response = api_instance.get_experiment_templates_v1_experiment_templates_get(
-                    include_mine=include_mine, include_approved=include_approved, offset=offset, limit=limit
+                    query=query,
+                    mine=mine, 
+                    approved=approved,
+                    public=public,
+                    finalized=finalized, 
+                    offset=offset, 
+                    limit=limit
                 )
                 return api_response
             except Exception as e:
@@ -143,4 +166,85 @@ class ExperimentsTemplates:
                 return api_response
             except Exception as e:
                 raise(f'Exception {e}')
+            
+
+    def remove(self,
+               id: str) -> None:
+        """
+            Remove specific experiment template specified by ID.
+            Args:
+                id (str): ID of experiment template to be removed.
+            Returns:
+                None.
+        """
+        with aiod_rail_sdk.ApiClient(self._configuration) as api_client:
+            api_instance = aiod_rail_sdk.ExperimentTemplatesApi(api_client)
+            
+            try:
+                api_instance.remove_experiment_template_v1_experiment_templates_id_delete(id)
+            except Exception as e:
+                raise(f'Exception {e}')
+
+    def archive(self,
+                id: str,
+                archived: bool = False) -> None:
+        """
+            Archives specific experiment template specified by ID.
+            Args:
+                id (str): ID of experiment template to be archived.
+                archived (bool): If experiment template should be archived or un-archived. Defaults to False.
+            Returns:
+                None.
+        """
+        with aiod_rail_sdk.ApiClient(self._configuration) as api_client:
+            api_instance = aiod_rail_sdk.ExperimentTemplatesApi(api_client)
+            
+            try:
+                api_instance.archive_experiment_template_v1_experiment_templates_id_archive_patch(id=id, archived=archived)
+            except Exception as e:
+                raise(f'Exception {e}')
+
+    def update(self,
+               id: str,
+               file: dict) -> aiod_rail_sdk.ExperimentTemplateResponse:
+        """
+            Updates specific experiment template.
+            Args:
+                id (str): ID of experiment template to be updated.
+                file: (Union[dict, tuple[str, str, str, dict]]): The file can be passed either as full specified json (dictionary)
+                                                                 or as a tuple of three strings and a json (dictionary) specifying 
+                                                                 the paths to script, requirements and docker image in this order 
+                                                                 and template description (name, description, task etc.).
+            Returns:
+                aiod_rail_sdk.ExperimentTemplateResponse: Updated Experiment template by given ID.
+        """
+        json_data = None
+        if isinstance(file, dict):
+            json_data = json.dumps(file)
+        
+        elif isinstance(file, tuple) and len(file) == 4 and all(isinstance(item, (str, dict)) for item in file):
+            path_script, path_requirements, path_image, config = file
+            if isinstance(config, dict):
+                with open(path_script, 'r') as s, open(path_requirements, 'r') as r, open(path_image, 'r') as i:
+                    script = s.read()
+                    requirements = r.read()
+                    image = i.read()
+                    config.update({'script': script, 'pip_requirements': requirements, 'base_image': image})
+                    json_data = json.dumps(config)
+            else:
+                raise ValueError("Fourth element must be a dictionary")
+        else:
+            raise ValueError("Invalid input format")
+        
+        experiment_template_create_instance = aiod_rail_sdk.ExperimentTemplateCreate.from_json(json_data)
+
+        with aiod_rail_sdk.ApiClient(self._configuration) as api_client:
+            api_instance = aiod_rail_sdk.ExperimentTemplatesApi(api_client)
+            
+            try:
+                api_response = api_instance.update_experiment_template_v1_experiment_templates_id_put(id, experiment_template_create=experiment_template_create_instance)
+                return api_response
+            except Exception as e:
+                raise(f'Exception {e}')
+
     
