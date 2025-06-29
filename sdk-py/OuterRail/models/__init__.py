778976a8c7211f21b72411fd3b53e981c03961a9
BarutJozef <xbarut@stuba.sk>
2025-06-30 00:45:05 +0200

    importing models

---
+++ sdk-py/OuterRail/models/__init__.py
@@ -0,0 +1,7 @@
+from OuterRail.models.asset_schema import AssetSchema
+from OuterRail.models.asset_cardinality import AssetCardinality
+from OuterRail.models.environment_var import EnvironmentVar
+from OuterRail.models.environment_var_def import EnvironmentVarDef
+from OuterRail.models.task_type import TaskType
+
+__all__ = ["AssetSchema", "AssetCardinality", "EnvironmentVar", "EnvironmentVarDef", "TaskType"]
